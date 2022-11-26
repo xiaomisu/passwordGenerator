a82dfe66275be710c63ed7a437bd8011ea8706ac
郑智文 <zhengzhiwen@bolegames.com>
2022-11-26 11:58:43 +0800

    feat(password generate):generate password

--- learn_react_ts/src/App.tsx
+++ learn_react_ts/src/App.tsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState<any[]>([]);
+  const [password, setPassword] = useState("**********************");
+  const generatePassword = async () => {
+    // Create a random password
+    const randomPassword =
+      Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
 
+    // Set the generated password as state
+    setPassword(randomPassword);
+
+    // copy the password to the clipboard & fire toast message
+
+    navigator.clipboard.writeText(randomPassword).then((res) => {
+      console.log(res);
+    });
+
+    if (true) {
+      toast("Password copied to your clipboard");
+    }
+  };
   return (
-    <div className="App">
-      <span className="heading">Taskify</span>
+    <div className="background">
+      <h1 className="heading">Generate a random password</h1>
+      <div className="wrapper">
+        <div className="password">**********************</div>
+        <button className="generate-password" onClick={generatePassword}>
+          Generate password
+        </button>
+      </div>
+      <ToastContainer />
     </div>
   );
 };
